46e1231e71c8297c53bc68455239684322e2ba66
Matteo Agostini <matteo.agostini@ph.tum.de>
2018-01-03 12:26:02 +0100

    fixed linkdef to follow root6 syntax. Added also "!" to avoid creation of operator >>

--- src/template-project_LinkDef.h
+++ src/template-project_LinkDef.h
@@ -1,11 +1,11 @@
 #ifndef template_project_LinkDef_h
 #define template_project_LinkDef_h
 
-#ifdef __CINT__
+#ifdef __CLING__
 
 // HelloWorld.h
-#pragma link C++ class tmplns::HelloWorld-;
+#pragma link C++ class tmplns::HelloWorld-!;
 
-#endif // __CINT__
+#endif // __CLING__
 
 #endif // template_project_LinkDef_h
